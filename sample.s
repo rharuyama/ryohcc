eea0f87c207a1441d10cd85e77c31f1d548348ba
rharuyama <aprilfoolsd1y@yahoo.co.jp>
2021-05-27 14:41:16 +0900

    sample assembly: a = 3; b = 4; a + b; => 7

--- sample.s
+++ sample.s
@@ -1,9 +1,24 @@
 .intel_syntax noprefix
 .globl main
 main:	
+	push rbp
 	mov rbp, rsp
+	sub rsp,208
 	
-	mov QWORD PTR [rbp-8], 42
-	mov rax, QWORD PTR [rbp-8]
+	mov QWORD PTR [rbp-8], 3 // a = 3;
+	mov QWORD PTR [rbp-16], 4 // b = 4;
 
+	// a + b;
+	push [rbp-8]
+	push [rbp-16]
+
+	pop rax
+	pop rdi
+	add rax, rdi
+	push rax
+
+	pop rax
+
+	mov rsp, rbp
+	pop rbp
 	ret
